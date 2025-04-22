a9be290ccea111ea49226f21a4e5d966f7045843
PrathamM16 <pratham.m1605@gmail.com>
2025-04-22 22:43:38 +0530

    check for jenkins to fail build

--- TRIVIUMBUYS-FRONTEND/src/main.ts
+++ TRIVIUMBUYS-FRONTEND/src/main.ts
@@ -2,17 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { provideAnimations } from '@angular/platform-browser/animations';
-// import { provideToastr } from 'ngx-toastr'; // ✅ Add this for Toastr
-import { provideToastrWrong } from 'ngx-toastr';
+import { provideToastr } from 'ngx-toastr'; // ✅ Add this for Toastr
 
+throw new Error('Forcefully failing Jenkins build');
+hi what to do for this error 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []), // keep your old providers also
     provideAnimations(),            // ✅ Needed for ngx-toastr
-    // provideToastr() ,
-    provideToastrWrong
-                     // ✅ Needed for ngx-toastr
+    provideToastr()                  // ✅ Needed for ngx-toastr
   ]
 })
 .catch(err => console.error(err));
